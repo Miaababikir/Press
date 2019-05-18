db5a1cad0a4c296f93ef0cda4350f667ce58c416
Miaababikir <miaababikir@gmail.com>
2019-05-18 21:57:42 +0200

    Setting the project

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace miaababikir\Press\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class InitialTest extends TestCase
+{
+
+    /** @test */
+    public function testing()
+    {
+        $this->assertTrue(true);
+    }
+
+}
